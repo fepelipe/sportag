7a58e06898d4cbb7c85462e3d71eef2d24e8a6e5
fepelipe <cabralff@gmail.com>
2014-02-18 22:42:51 -0400

    Dialog com lista de Esportes. (tem que testar ainda)

--- Sportag/src/br/ufam/sportag/dialog/EventFilterDialog.java
+++ Sportag/src/br/ufam/sportag/dialog/EventFilterDialog.java
@@ -1,13 +1,33 @@
 package br.ufam.sportag.dialog;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import br.ufam.sportag.R;
 
 public class EventFilterDialog extends DialogFragment {
-	
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		return super.onCreateDialog(savedInstanceState);
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setTitle("Filtre os resultados")
+				.setNegativeButton("Limpar Filtro", new OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+
+					}
+				})
+				.setItems(R.array.sports_array,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+
+							}
+						});
+		return builder.create();
 	}
 }
